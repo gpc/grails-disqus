4b5b5d4caa785dc0aa6303e8a89d8a2604a5bd50
Lari Hotari <lhotari@pivotal.io>
2015-01-15 08:50:37 -0500

    bump version

--- DisqusGrailsPlugin.groovy
+++ DisqusGrailsPlugin.groovy
@@ -2,9 +2,9 @@ import grails.util.Holders
 
 class DisqusGrailsPlugin {
     // the plugin version
-    def version = "0.3-SNAPSHOT"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0.0 > *"
+    def grailsVersion = "2.3.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
